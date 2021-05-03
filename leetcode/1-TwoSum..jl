6935389bfdfff7acc09d2cf940a63c1c6bc70f11
Ashwani Rathee <ab669522@gmail.com>
2021-05-03 17:30:06 +0530

    algorithms section created

---
+++ leetcode/1-TwoSum..jl
@@ -0,0 +1,29 @@
+"""
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of 
+the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Constraints:
+
+- 2 <= nums.length <= 103
+- -109 <= nums[i] <= 109
+- -109 <= target <= 109
+- Only one valid answer exists.
+"""
+function two_sum(nums::AbstractArray,target::Int)
+    
+end
+
+
+# Arguments
+nums = [2,7,11,15]
+target = 9
+
+# Function call
+two_sum(nums,target)
--- test.jl
+++ test.jl
@@ -1,15 +1,23 @@
 # 
 
-function missingnum(array::AbstractArray, n::Tuple{Int64})
-    # println(array, n);
-    total = ( n[1] + 1) * ( n[1] + 2) / 2; # 
-    for i in 1:n[1]
-        total = total - array[i];
+"""
+
+
+"""
+function merge_sort(arr::AbstractArray{T,1}, l::T, r::T) where {T <: Real}
+    if r > l 
+        mid = Int(ceil(l + (r-l)/2))
+        println(mid)
+        merge_sort(arr, l, mid)
+        merge_sort(arr, mid, r)
     end
-    return total;
+    return "Hello"
 end
 
-arr = [8, 7, 6, 5, 4, 3, 1] # 2 is missing
-n = size(arr) # returns dimension wise size tuple  
-
-missingnum(arr, n) # main function
+# Arguments
+arr = [5 ,3, 1, 4, 2]
+n = size(arr)[1]
+l = 0
+r = n
+# Function call
+merge_sort(arr, l, r)
