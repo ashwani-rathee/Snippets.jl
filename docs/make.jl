017a5185d01e4fdc2a7613b77bd3b80b919561df
Ashwani Rathee <ab669522@gmail.com>
2021-05-05 17:28:09 +0530

    gh-pages setup

--- docs/make.jl
+++
@@ -1,9 +0,0 @@
-push!(LOAD_PATH,"../src/")
-using Documenter
-# Inside make.jl
-
-makedocs(sitename="Documentation",
-        pages=[
-            "Home" => "index.md"
-           ])
-deploydocs(repo = "github.com/ashwani-rathee/Snippets.jl")
--- docs/src/index.md
+++
@@ -1,8 +0,0 @@
-# VegaGraphs.jl
-*The best summation package. 1*
-## Package Features
-- Sum the squares of two numbers
-## Function Documentation
-```@docs
-MyFunction
-```
