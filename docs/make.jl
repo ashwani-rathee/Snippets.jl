399d477c9df909d18d5d25ee80fe2be283d9a2b2
Ashwani Rathee <ab669522@gmail.com>
2021-05-05 08:40:43 +0530

    make.jl cleanup

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ using Documenter, ImageDraw
 
 makedocs(sitename="Documentation",
             format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"))
-deploydocs(repo = "github.com/JuliaImages/ImageDraw.jl.git")
+deploydocs(repo = "github.com/ashwani-rathee/Snippets.jl.git")
