028cd3ffe536e2083a69e832b041b3cfc6e09af1
Ashwani Rathee <ab669522@gmail.com>
2021-05-05 12:08:20 +0530

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,7 @@
 using Documenter,ImageDraw
+# Inside make.jl
+push!(LOAD_PATH,"../src/")
 
 makedocs(sitename="Documentation",
             format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"))
-deploydocs(repo = "github.com/ashwani-rathee/Snippets.jl.git")
+deploydocs(repo = "github.com/ashwani-rathee/Snippets.jl")
--- src/Snippets.jl
+++ src/Snippets.jl
@@ -2,4 +2,17 @@ module Snippets
 
 greet() = print("Hello World!")
 
+# Example of function inside ./src/VegaGraphs.jl
+"""
+    MyFunction(x,y)
+This is an example of Docstring. This function receives two 
+numbers x and y and returns the sum of the squares.
+```math
+x^2 + y^2 
+```
+"""
+function MyFunction(x,y)
+    return x^2+y^2
+end
+
 end # module
